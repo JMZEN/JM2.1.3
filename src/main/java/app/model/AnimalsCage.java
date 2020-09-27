7a6120eea814e984f5c27caf5d0a4a8698063dd5
JMZEN <lamp507@yandex.ru>
2020-09-27 21:32:51 +0300

    All times are the same now.

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -4,13 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import java.sql.Timestamp;
-
 @Component
 public class AnimalsCage {
 
     @Autowired
-    @Qualifier(value = "dog")
+    @Qualifier("dog")
     private Animal animal;
 
     @Autowired
@@ -28,7 +26,7 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
